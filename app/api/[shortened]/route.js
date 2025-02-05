caaf6fee3ec4886ab77fce2dd8a769a78111b83d
Nimesh Piyumal <nimeshofficial.info@gmail.com>
2025-02-05 17:31:31 +0000

    -----------

--- app/api/[shortened]/route.js
+++ app/api/[shortened]/route.js
@@ -2,10 +2,8 @@ import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-export async function GET(req, context) {
-  // Await the params before destructuring
-  const params = await context.params;
-  const { shortened } = params; // Now you can safely destructure the 'shortened' property
+export async function GET(request, { params }) {
+  const { shortened } = params;
 
   try {
     await client.connect();
@@ -18,17 +16,23 @@ export async function GET(req, context) {
       return new Response(null, {
         status: 301,
         headers: {
-          Location: url.originalUrl, // Redirect to the original URL
+          Location: url.originalUrl,
         },
       });
     } else {
       return new Response(JSON.stringify({ error: 'URL not found' }), {
         status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       });
     }
   } catch (error) {
     return new Response(JSON.stringify({ error: 'Something went wrong' }), {
       status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
   } finally {
     await client.close();
